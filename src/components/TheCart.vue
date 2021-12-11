99664a023e0d4301bb3e9dfc294c38c246b1389d
Karol <karol.chamski@gmail.com>
2021-12-11 14:14:45 +0100

    added modal if cart is empty

--- src/components/TheCart.vue
+++ src/components/TheCart.vue
@@ -1,12 +1,18 @@
 <template>
 <div class="wrapper">
+    
     <div class="the-cart">
         <div v-for="item in cart" :key="item.title">
             <p>{{item.title}}</p>
             <p>{{item.price * item.counter}}zł</p>
             <img :src="`/img/${item.img}`" alt="">
-            <p>ilość:{{item.counter}}</p>
+            <p>ilość: {{item.counter}}</p>
         </div>
+
+        <div v-if="this.cart.length == 0" class="empty-cart">
+        <p>Koszyk jest pusty</p>
+        </div>
+
     </div>
 </div>
 </template>
@@ -18,7 +24,7 @@ export default{
         totalPrice(item){
             return item.price * item.counter
         }
-    }
-    
+    },
+
 }
 </script>
